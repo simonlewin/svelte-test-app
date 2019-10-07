b7a6d30e39bef58f94636f4abbf5ab50578fc00f
Simon Lewin <simonlewin@hotmail.com>
2019-10-07 15:53:10 +0100

    Add launch card with background image
    - add launch card <div />
    - add background image to card using getBackgroundImage function to get either galaxy.jpg, iss.jpg or space.jpg from image assets
    - add styling and use rocket and mission name

--- src/Launch.svelte
+++ src/Launch.svelte
@@ -1,8 +1,43 @@
 <script>
   export let launch = {};
 
-  const { id = null } = launch;
-  const { mission: { name = '' } = {} } = launch;
+  const { id } = launch;
+  const { rocket, mission } = launch;
+
+  const backgrounds = ['galaxy', 'iss', 'space'];
+
+  const getBackgroundImage = id =>
+    `background-image: url(/assets/images/${
+      backgrounds[Number(id) % backgrounds.length]
+    }.jpg)`;
+
+  $: image = getBackgroundImage(id);
 </script>
 
-<li>{id}. {name}</li>
+<style>
+  div {
+    padding: 32px 40px;
+    border-radius: 7px;
+    margin-bottom: 24px;
+    background-size: cover;
+  }
+
+  h3 {
+    color: azure;
+    font-size: 36px;
+    font-weight: 600;
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  h5 {
+    color: azure;
+    font-size: 16px;
+    text-transform: uppercase;
+    letter-spacing: 4px;
+  }
+</style>
+
+<div style={image}>
+  <h3>{mission.name}</h3>
+  <h5>{rocket.name}</h5>
+</div>
