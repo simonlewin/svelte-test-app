17a9b51d1a9457143c3e5a145382d48fdbe8dd05
Simon Lewin <simonlewin@hotmail.com>
2019-10-09 14:26:26 +0100

    Add Button and Bar components and component index
    - add Button component with on: directive without a value so the component will forward the event
    - add simple top Bar component
    - add index to handle component exports

---
+++ src/components/Bar.svelte
@@ -0,0 +1,9 @@
+<style>
+  div {
+    flex-shrink: 0;
+    height: 12px;
+    background-color: var(--primary);
+  }
+</style>
+
+<div />
---
+++ src/components/Button.svelte
@@ -0,0 +1,36 @@
+<style>
+  :root {
+    --height: 50px;
+  }
+
+  button {
+    display: block;
+    min-width: 200px;
+    height: var(--height);
+    margin: 0 auto;
+    padding: 0 calc(var(--unit) * 4);
+    border: none;
+    border-radius: calc(var(--height) / 2);
+    font-family: inherit;
+    font-size: 18px;
+    line-height: var(--height);
+    font-weight: 700;
+    color: white;
+    text-transform: uppercase;
+    background-color: var(--accent);
+    cursor: pointer;
+    outline: none;
+  }
+
+  button:hover {
+    filter: opacity(90%);
+  }
+
+  button:active {
+    filter: opacity(80%);
+  }
+</style>
+
+<button on:click>
+  <slot />
+</button>
