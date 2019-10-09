732fb6a60eb5f5f26889dc73015df7382f7240a5
Simon Lewin <simonlewin@hotmail.com>
2019-10-09 18:32:23 +0100

    Add <LaunchDetail /> component
    - add detail - rocket name, site, etc. from launch prop
    - add styling for launch detail container
    - add background image (from helper)

---
+++ src/components/LaunchDetail.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { getBackgroundImage } from '../helpers/backgrounds';
+
+  export let launch;
+
+  const { date, id, rocket, site } = launch;
+
+  const launchDate = new Date(date);
+</script>
+
+<style>
+  div {
+    padding: calc(var(--unit) * 4) calc(var(--unit) * 5);
+    border-radius: 7px;
+    color: white;
+    background-size: cover;
+    background-position: center;
+    height: 365px;
+    margin-bottom: calc(var(--unit) * 4);
+  }
+</style>
+
+<div style={getBackgroundImage(id)}>
+  <h3>{rocket.name} ({rocket.type})</h3>
+  <h5>{site}</h5>
+  <h6>{`${launchDate.getFullYear()}`}</h6>
+</div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button.svelte';
 export { default as Header } from './Header.svelte';
 export { default as Bar } from './Bar.svelte';
 export { default as LaunchTile } from './LaunchTile.svelte';
+export { default as LaunchDetail } from './LaunchDetail.svelte';
