8f5db52d4be8f5b648e55734d267b83332ebee50
Simon Lewin <simonlewin@hotmail.com>
2019-10-10 16:58:33 +0100

    Add Home, Cart, etc. SVG icons
    - add SVGs for each icon
    - add styling for SVGs
    - add icon index

---
+++ src/components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as CartIcon } from './CartIcon.svelte';
+export { default as HomeIcon } from './HomeIcon.svelte';
+export { default as LogoutIcon } from './LogoutIcon.svelte';
+export { default as ProfileIcon } from './ProfileIcon.svelte';
