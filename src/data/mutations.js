118cd1cadf0148f66bcaa88790a3531a8f29aeae
Simon Lewin <simonlewin@hotmail.com>
2019-10-21 20:53:02 +0100

    Add ability to book trips
    - add container for BookTrips action button
    - add BookTrips to container index
    - add BookTrips mutation (together with CancelTrip mutation)

--- src/data/mutations.js
+++ src/data/mutations.js
@@ -5,3 +5,29 @@ export const TOGGLE_CART = gql`
     addOrRemoveFromCart(id: $launchId) @client
   }
 `;
+
+export const BOOK_TRIPS = gql`
+  mutation BookTrips($launchIds: [ID]!) {
+    bookTrips(launchIds: $launchIds) {
+      success
+      message
+      launches {
+        id
+        isBooked
+      }
+    }
+  }
+`;
+
+export const CANCEL_TRIP = gql`
+  mutation cancel($launchId: ID!) {
+    cancelTrip(launchId: $launchId) {
+      success
+      message
+      launches {
+        id
+        isBooked
+      }
+    }
+  }
+`;
