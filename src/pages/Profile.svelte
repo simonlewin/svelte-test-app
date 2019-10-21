12b67b3e5faa6608189f7274fc93ce2d15198cb8
Simon Lewin <simonlewin@hotmail.com>
2019-10-21 14:24:04 +0100

    Add functionality to get and list trips
    - add query to get trips
    - add await and each block to list trips

--- src/pages/Profile.svelte
+++ src/pages/Profile.svelte
@@ -1,5 +1,31 @@
 <script>
-  import { Header } from '../components';
+  import { onMount } from 'svelte';
+  import { getClient, query } from 'svelte-apollo';
+
+  import { Header, LaunchTile, Loading } from '../components';
+
+  import { GET_MY_TRIPS } from '../data/queries';
+
+  const client = getClient();
+
+  let trips = [];
+
+  const getTrips = query(client, { query: GET_MY_TRIPS });
+
+  onMount(async () => {
+    const { data } = await $getTrips;
+    trips = data.me.trips;
+  });
 </script>
 
 <Header>My Trips</Header>
+
+{#if trips.length === 0}
+  <Loading />
+{:else}
+  {#each trips as launch}
+    <LaunchTile {launch} />
+  {:else}
+    <p>>You haven't booked any trips</p>
+  {/each}
+{/if}
